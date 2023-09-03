0e2b4b595863514b4a7121d8b29c471b1ffd8045
mahmoud khames <khamesh680@gmail.com>
2023-09-03 21:36:15 +0300

    pro

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -7,6 +7,7 @@ import Github from "../component/images/screencapture-h2d0-github-io-Github-Prof
 import todo from '../component/images/screencapture-h2d0-github-io-Todo-List-2023-08-08-21_04_33.png'
 import Listen from "../component/images/screencapture-h2d0-github-io-Listen-2023-08-08-17_41_31.png";
 import Game from "../component/images/screencapture-h2d0-github-io-Game-2023-08-08-21_05_11.png";
+import Movies from "../component/images/screencapture-h2d0-github-io-movieslove-2023-09-03-18_17_11.png";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
@@ -24,7 +25,59 @@ function Projects() {
               <div className="cont-text">
                 <h2 className="projects-title col-12">Projects.</h2>
               </div>
-              <div className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"data-aos="fade-right">
+              <div
+                className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"
+                data-aos="fade-right"
+              >
+                <div className="custom-block  ">
+                  <div className="img-project">
+                    <img
+                      src={Movies}
+                      className="custom-block-image img-fluid"
+                      alt=""
+                    />
+                  </div>
+                  <div className="p-text">
+                    <p className="custom-block-p">Website</p>
+                  </div>
+                  <div className="d-flex">
+                    <div>
+                      <h5 className="custom-block-title text-black mb-2">
+                        Movie Love Website
+                      </h5>
+                      <p className="custom-p mb-0">
+                        "Movie Love" is a website that allows users to browse
+                        movies and TV shows, view their details, watch trailers,
+                        and see photos and posters. •Movies and TV Shows
+                        Details, Trailers, Photos, Posters, Recommendations,
+                        Trending Movies and TV Shows, and more. •Search for a
+                        Movie or TV Show by its name •Click on any movie or tv
+                        show to open its full details •Trailers, Photos,
+                        Posters, Recommendations
+                      </p>
+                      <a
+                        target="__blank"
+                        href="https://github.com/H2d0/movieslove"
+                        className="icon text-decoration-none"
+                      >
+                        <i className="fa-brands fa-github github-icon"></i>
+                        <span>view</span>
+                      </a>
+                    </div>
+                    <a
+                      target="__blank"
+                      href=" https://h2d0.github.io/movieslove/"
+                      className="custom-block-icon bg-design  ms-auto"
+                    >
+                      <i className="fa-solid fa-location-arrow"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"
+                data-aos="fade-right"
+              >
                 <div className="custom-block  ">
                   <div className="img-project">
                     <img
@@ -65,7 +118,10 @@ function Projects() {
                   </div>
                 </div>
               </div>
-              <div className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"data-aos="fade-left">
+              <div
+                className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"
+                data-aos="fade-left"
+              >
                 <div className="custom-block  ">
                   <div className="img-project">
                     <img
@@ -106,7 +162,10 @@ function Projects() {
                   </div>
                 </div>
               </div>
-              <div className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"data-aos="fade-right">
+              <div
+                className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"
+                data-aos="fade-right"
+              >
                 <div className="custom-block  ">
                   <div className="img-project">
                     <img
@@ -147,7 +206,10 @@ function Projects() {
                   </div>
                 </div>
               </div>
-              <div className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"data-aos="fade-left">
+              <div
+                className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"
+                data-aos="fade-left"
+              >
                 <div className="custom-block  ">
                   <div className="img-project">
                     <img
@@ -188,7 +250,10 @@ function Projects() {
                   </div>
                 </div>
               </div>
-              <div className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"data-aos="fade-right">
+              <div
+                className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"
+                data-aos="fade-right"
+              >
                 <div className="custom-block  ">
                   <div className="img-project">
                     <img
@@ -229,7 +294,10 @@ function Projects() {
                   </div>
                 </div>
               </div>
-              <div className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"data-aos="fade-left">
+              <div
+                className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"
+                data-aos="fade-left"
+              >
                 <div className="custom-block  ">
                   <div className="img-project">
                     <img
@@ -270,7 +338,10 @@ function Projects() {
                   </div>
                 </div>
               </div>
-              <div className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"data-aos="fade-right">
+              <div
+                className="col-lg-4 col-md-6 col-12 mb-4 mb-4 mb-lg-0"
+                data-aos="fade-right"
+              >
                 <div className="custom-block  ">
                   <div className="img-project">
                     <img
