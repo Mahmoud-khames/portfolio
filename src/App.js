c0861998b885283cc673780be80c03f5928f9832
mahmoud khames <khamesh680@gmail.com>
2023-09-03 21:42:00 +0300

    protfolio

--- src/App.js
+++ src/App.js
@@ -6,16 +6,25 @@ import Navbar from './component/Navbar';
 import Projects from './component/Projects';
 import ConcatMe from './component/Concat-Me';
 import Footer from './component/Footer';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      
+      <Routes>
+        <Route path='/' element={
+          <>
+        <Navbar />
       <Home />
       <AboutMe />
       <Projects />
       <ConcatMe />
       <Footer/>
+        </>} />
+      </Routes>
+      
+      
     </div>
   );
 }
